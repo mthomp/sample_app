d89b1c656f9f9c5007bc03559f595ee9630598f6
Matt Thompson <mthomp4@verizon.net>
2011-06-17 12:54:04 -0400

    Fixed crlf

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ SampleApp::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+    
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
